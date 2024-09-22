6d7b364469043c5a91850afd919b20a181df6fe5
Ivailo_Angelov <ivoangelov8699@gmail.com>
2024-09-22 23:35:00 +0300

    .

--- NewApp/src/app/app.routes.ts
+++ NewApp/src/app/app.routes.ts
@@ -1,6 +1,10 @@
 import { Routes } from '@angular/router';
 import { HomePageComponent } from './home-page/home-page.component';
+import { RegisterComponent } from './user/register/register.component';
+import { LogInComponent } from './user/log-in/log-in.component';
 
 export const routes: Routes = [
-    { path: "app-home-page",component:HomePageComponent,}
+    { path: "app-home-page",component:HomePageComponent,},
+    {path: "app-register",component:RegisterComponent},
+    {path:"app-log-in",component:LogInComponent}
 ];
